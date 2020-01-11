1eb3627421ebb2f65359af8387eaf49304fb4eb6
jprous85 <jprous85@gmail.com>
2020-01-11 20:12:27 +0100

    reorganization of code for better response

--- user.go
+++ user.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm"
 	"net/http"
@@ -13,12 +12,12 @@ import (
 var db = connect()
 
 type User struct {
-	Name            string `json:"name"`
-	First_last_name string `json:"first_last_name"`
-	Email           string `json:"email"`
-	CreatedAt       time.Time   `json:"created_at"`
-	UpdatedAt       time.Time   `json:"updated_at"`
-	DeletedAt       *time.Time  `json:"deleted_at"`
+	Name            string     `json:"name"`
+	First_last_name string     `json:"first_last_name"`
+	Email           string     `json:"email"`
+	CreatedAt       time.Time  `json:"created_at"`
+	UpdatedAt       time.Time  `json:"updated_at"`
+	DeletedAt       *time.Time `json:"deleted_at"`
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
@@ -44,7 +43,7 @@ func create(writer http.ResponseWriter, r *http.Request) {
 	firstLastName := r.FormValue("first_last_name")
 	email := r.FormValue("email")
 
-	db.Create(&User{
+	result := db.Create(&User{
 		Name:            name,
 		First_last_name: firstLastName,
 		Email:           email,
@@ -53,9 +52,11 @@ func create(writer http.ResponseWriter, r *http.Request) {
 		DeletedAt:       nil,
 	})
 
-	fmt.Fprintln(writer, "New user created successfully!")
+	if result.Error != nil {
+		json.NewEncoder(writer).Encode(map[string]string{"data": "ok"})
+	}
+	json.NewEncoder(writer).Encode(map[string]string{"data": "ko"})
 }
-
 
 func update(writer http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
@@ -70,21 +71,28 @@ func update(writer http.ResponseWriter, request *http.Request) {
 	firstLastName := request.FormValue("first_last_name")
 	email := request.FormValue("email")
 
-	db.Model(&user).Where("id = ?", id).Update(&User{
+	result := db.Model(&user).Where("id = ?", id).Update(&User{
 		Name:            name,
 		First_last_name: firstLastName,
 		Email:           email,
 		UpdatedAt:       time.Now().UTC(),
 	})
 
-	fmt.Fprintln(writer, "User updated successfully!")
+	if result.Error != nil {
+		json.NewEncoder(writer).Encode(map[string]string{"data": "ok"})
+	}
+	json.NewEncoder(writer).Encode(map[string]string{"data": "ko"})
 }
 
 func delete(writer http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
 	id := vars["id"]
 	var user User
-	db.Where("id = ?", id).Delete(&user)
 
-	fmt.Fprintln(writer, "User delete successfully!")
+	result := db.Where("id = ?", id).Unscoped().Delete(&user)
+
+	if result.Error != nil {
+		json.NewEncoder(writer).Encode(map[string]string{"data": "ok"})
+	}
+	json.NewEncoder(writer).Encode(map[string]string{"data": "ko"})
 }
